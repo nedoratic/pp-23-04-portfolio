caa4cbbba461bcb12d3775f995e7c494984cb3af
nedoratic <nedoratic@gmail.com>
2023-12-21 16:33:55 +0100

    added style and toggle functionality to menu list

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuBtnBurger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
 
@@ -9,8 +12,14 @@ function toggleMenu() {
 	if (!showMenu) {
 		menuBtnBurger.classList.add("open");
 		showMenu = true;
+		nav.classList.add("open");
+		menuNav.classList.add("open");
+		navItems.forEach((item) => item.classList.add("open"));
 	} else {
 		menuBtnBurger.classList.remove("open");
 		showMenu = false;
+		nav.classList.remove("open");
+		menuNav.classList.remove("open");
+		navItems.forEach((item) => item.classList.remove("open"));
 	}
 }
